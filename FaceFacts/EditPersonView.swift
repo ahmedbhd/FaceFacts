1636e8eacd96badd861caf085d718fd1a94ec5cb
Ahmed Ben Henda <ahmed.bnhenda@gmail.com>
2024-02-26 10:21:32 +0100

    Edit action

---
+++ FaceFacts/EditPersonView.swift
@@ -0,0 +1,37 @@
+//
+//  EditPersonView.swift
+//  FaceFacts
+//
+//  Created by Anypli M1 Air on 26/2/2024.
+//
+
+import SwiftUI
+
+struct EditPersonView: View {
+    // ObservableObject is replaced by Observale in macro
+    // @Binding: is used for local references to external state
+    @Bindable var person: Person // Bindable: observable macro object type to create bindings from it
+    
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $person.name)
+                    .textContentType(.name)
+                
+                TextField("Email Address", text: $person.email)
+                    .textContentType(.emailAddress)
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+                
+                TextField("Details about this person", text: $person.details, axis: .vertical)
+            }
+        }
+        .navigationTitle("Edit Person")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+//#Preview {
+//    EditPersonView(person: <#T##Person#>)
+//}
